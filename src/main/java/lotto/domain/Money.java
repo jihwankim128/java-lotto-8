9549eb8d9e0a002fd5a30f096ea3c412951a107e
jihwankim128 <jihwan1535@nate.com>
2025-11-01 02:56:52 +0900

    feat: 금액 1000원 단위 검증 기능 추가

--- src/main/java/lotto/domain/Money.java
+++ src/main/java/lotto/domain/Money.java
@@ -2,6 +2,8 @@ package lotto.domain;
 
 public class Money {
 
+    private static final int UNIT = 1000;
+
     public Money(int money) {
         validateMoney(money);
     }
@@ -9,6 +11,9 @@ public class Money {
     private static void validateMoney(int money) {
         if (money < 0) {
             throw new IllegalArgumentException("금액은 음수일 수 없습니다.");
+        }
+        if (money % UNIT != 0) {
+            throw new IllegalArgumentException("금액은 %s원 단위여야 합니다.".formatted(UNIT));
         }
     }
 }
--- src/test/java/lotto/domain/MoneyTest.java
+++ src/test/java/lotto/domain/MoneyTest.java
@@ -3,6 +3,8 @@ package lotto.domain;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class MoneyTest {
 
@@ -15,5 +17,14 @@ class MoneyTest {
         assertThatThrownBy(() -> new Money(negativeMoney))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("금액은 음수일 수 없습니다.");
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {999, 1001})
+    void 천원_단위가_아니라면_예외가_발생한다(int invalidMoney) {
+        // when & then
+        assertThatThrownBy(() -> new Money(invalidMoney))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("금액은 1000원 단위여야 합니다.");
     }
 }

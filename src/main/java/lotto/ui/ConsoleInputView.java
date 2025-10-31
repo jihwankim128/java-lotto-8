e68f29c3178cf4b83409c15f04bfcabec9b916d9
jihwankim128 <jihwan1535@nate.com>
2025-10-31 23:55:36 +0900

    refactor: 입력 로직 공통 메서드 관심사 분리

--- src/main/java/lotto/ui/ConsoleInputView.java
+++ src/main/java/lotto/ui/ConsoleInputView.java
@@ -8,20 +8,30 @@ public class ConsoleInputView {
 
     public int readPurchaseAmount() {
         System.out.println("구입금액을 입력해 주세요.");
-        return Integer.parseInt(Console.readLine());
+        return parseNumber(Console.readLine());
     }
 
     public List<Integer> readWinningNumbers() {
         System.out.println("당첨 번호를 입력해 주세요.");
-        String winningNumbers = Console.readLine();
-        System.out.println();
-        return Arrays.stream(winningNumbers.split(","))
-                .map(Integer::parseInt)
-                .toList();
+        return readNumbers();
     }
 
     public int readBonusNumber() {
         System.out.println("보너스 번호를 입력해 주세요.");
-        return Integer.parseInt(Console.readLine());
+        return parseNumber(Console.readLine());
+    }
+
+    private List<Integer> readNumbers() {
+        return Arrays.stream(Console.readLine().split(","))
+                .map(this::parseNumber)
+                .toList();
+    }
+
+    private int parseNumber(String textNumber) {
+        try {
+            return Integer.parseInt(textNumber.strip());
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 정수 형식이 아닙니다.");
+        }
     }
 }

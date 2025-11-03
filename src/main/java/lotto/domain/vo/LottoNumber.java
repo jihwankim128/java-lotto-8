a34e857ee44d79d77ea8387cc17b15084ff67282
jihwankim128 <jihwan1535@nate.com>
2025-11-03 19:11:04 +0900

    refactor: 로또 번호 값 범위 예외 메시지 상수화
    * 코드라인 감소 및 가독성 증가

--- src/main/java/lotto/domain/vo/LottoNumber.java
+++ src/main/java/lotto/domain/vo/LottoNumber.java
@@ -4,6 +4,8 @@ public record LottoNumber(int number) {
 
     public static final int MIN_RANGE = 1;
     public static final int MAX_RANGE = 45;
+    
+    private static final String INVALID_RANGE_FORMAT = "로또 번호는 %s부터 %s사이의 숫자여야 합니다.";
 
     public LottoNumber {
         validateLottoNumber(number);
@@ -11,9 +13,7 @@ public record LottoNumber(int number) {
 
     private void validateLottoNumber(int number) {
         if (number < MIN_RANGE || number > MAX_RANGE) {
-            throw new IllegalArgumentException(
-                    "로또 번호는 %s부터 %s사이의 숫자여야 합니다.".formatted(MIN_RANGE, MAX_RANGE)
-            );
+            throw new IllegalArgumentException(INVALID_RANGE_FORMAT.formatted(MIN_RANGE, MAX_RANGE));
         }
     }
 }
